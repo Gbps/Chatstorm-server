9f7b0c87f8692f15a9bf275b3a1f4e2e228eec67
Michael F <mjf364@utulsa.edu>
2014-10-25 09:56:18 -0600

    no message

--- json/getAllRooms.php
+++ json/getAllRooms.php
@@ -1,8 +1,8 @@
 <?php
 
-    require_once "chatstorm/chatstorm.php";
-    require_once "vendor/autoload.php";
-    require_once "generated-conf/config.php";
+    require_once "../chatstorm/chatstorm.php";
+    require_once "../vendor/autoload.php";
+    require_once "../generated-conf/config.php";
 
     $currentDate = new DateTime();
     $formatDate = $currentDate->format( "Y-m-d H:i:s" );
