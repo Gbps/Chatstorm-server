86c578e8ba3b31b8c5fcd57970db032cb5499ce8
Michael F <mjf364@utulsa.edu>
2014-10-25 09:25:39 -0600

    no message

--- test.php
+++ test.php
@@ -4,11 +4,19 @@
     require_once "vendor/autoload.php";
     require_once "generated-conf/config.php";
 
-$res = RegisteredUser::CreateUser( "test@test.com", "testpass", "imeiimeiimeiimei" );
 
+    $res = RegisteredUser::CreateUser( "gbps111@gmail.com", "testpass", "imeiimeiimeiimei" );
     if($res)
-        echo "User Created";
+        echo "User Created<br />";
     else
-        echo "Failed to create user.";
+        echo "Failed to create user.<br />";
+
+    $newUser = RegisteredUserQuery::create()->findByEmail("gbps111@gmail.com")->limit(1)->find();
+
+    $res = Room::CreateRoom( $newUser, "Some cool topic", 60000 );
+    if($res)
+        echo "Room Created<br />";
+    else
+        echo "Failed to create room.<br />";
 
 ?>
