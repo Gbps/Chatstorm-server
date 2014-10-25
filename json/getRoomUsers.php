74dffd4dbd58e333ac5b5cefde779238b3ca2125
Michael F <mjf364@utulsa.edu>
2014-10-25 14:22:36 -0600

    no message

---
+++ json/getRoomUsers.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once "../chatstorm/chatstorm.php";
+    require_once "../vendor/autoload.php";
+    require_once "../generated-conf/config.php";
+
+    use Chatstorm\Util as Util;
+
+    if( isset( $_GET['RoomId'] ) === false ) Util::DieWithJSONError("Argument missing in request.");
+
+    $roomId = intval( $_GET['RoomId'] );
+
+    $currentDate = new DateTime();
+    $formatDate = $currentDate->format( "Y-m-d H:i:s" );
+
+    $returnRoom = RoomQuery::create()->findOneByRoomid( $roomId );
+    if( $returnRoom == null )  Util::DieWithJSONError("Could not find room.");
+
+    $messages = $returnRoom->getRoomUsers()->toJson();
+
+    echo Util::ReturnJSON($messages);
+
