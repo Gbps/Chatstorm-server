4ca0cc9786bacbefe070bdb658855e8973b138c6
Michael F <mjf364@utulsa.edu>
2014-10-25 08:00:18 -0600

    no message

--- test.php
+++ test.php
@@ -2,8 +2,9 @@
 
     require_once "chatstorm/chatstorm.php";
     require_once "vendor/autoload.php";
+    require_once "/autoload.php";
 
-    $res = RegisteredUser::CreateUser( "test@test.com", "testpass", "imeiimeiimeiimei" );
+$res = RegisteredUser::CreateUser( "test@test.com", "testpass", "imeiimeiimeiimei" );
 
     if($res)
         echo "User Created";
