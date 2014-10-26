6a046b17651d8682f467aed72d873264e52ab3cf
Michael F <mjf364@utulsa.edu>
2014-10-25 23:26:13 -0600

    no message

--- chatstorm/chatstorm_validators.php
+++ chatstorm/chatstorm_validators.php
@@ -22,7 +22,7 @@ namespace Chatstorm
         public static function ValidateMessage( $message )
         {
             // Length <1024
-            if( strlen( $message ) > 1024 ) return false;
+            if( strlen( $message ) > 1024 || strlen( $message ) == 0 ) return false;
 
             return true;
         }
@@ -30,7 +30,7 @@ namespace Chatstorm
         public static function ValidateTopic( $topic )
         {
             // Length <124
-            if( strlen( topic ) > 124 ) return false;
+            if( strlen( $topic ) > 124 || strlen( $topic ) == 0 ) return false;
 
             return true;
         }
