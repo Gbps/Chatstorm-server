7459db825a33aedfef2c85a7187f335813626cbb
root <root@ChatAround.me>
2014-10-26 10:45:23 -0400

    sdfasdf

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit4507996194abdd4a9f4403d869eca396::getLoader();
+return ComposerAutoloaderInitdd3627c825a256d943ec8678ab3a1da2::getLoader();
